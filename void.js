50a2a9ad1dfd3f4f266548f88e7f31b585619be5
Mandala Shirisha <msiridec@gmail.com>
2025-07-24 23:19:43 +0530

    Add files via upload

---
+++ void.js
@@ -0,0 +1,17 @@
+function goToMenu() {
+    window.location.href = "menu.html";
+}
+
+function selectCategory(category) {
+    alert("Showing items from " + category);
+    window.location.href = "food.html";
+}
+
+function goToPayment() {
+    window.location.href = "payment.html";
+}
+
+function goHome() {
+    window.location.href = "index.html";
+}
+
